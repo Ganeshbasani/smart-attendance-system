cdd15839b6e86fd8e210dada3df1da64250c7802
Ganeshbasani <ganeshbasani43@gmail.com>
2025-12-24 17:46:10 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -6,7 +6,7 @@ ATTENDANCE_FILE = "attendance.csv"
 
 def load_students():
     students = []
-    with open(STUDENT_FILE, newline="") as f:
+    with open(STUDENT_FILE, newline="", encoding="utf-8") as f:
         reader = csv.DictReader(f)
         for row in reader:
             students.append(row)
@@ -14,8 +14,8 @@ def load_students():
 
 def save_attendance(attendance_data):
     today = date.today().isoformat()
-    with open(ATTENDANCE_FILE, "a", newline="") as f:
+
+    with open(ATTENDANCE_FILE, "a", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
         for student_id, status in attendance_data.items():
             writer.writerow([today, student_id, status])
-
